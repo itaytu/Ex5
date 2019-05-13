07db1afa79aec64a3c878115eb9bbf0273627b9a
itaytu <itaytu93@gmail.com>
2019-05-13 09:19:23 +0300

    range

--- chain.hpp
+++ chain.hpp
@@ -1,64 +1,87 @@
 #ifndef EX5_CHAIN_HPP
 #define EX5_CHAIN_HPP
 
-#include "range.hpp"
-
 namespace itertools {
-    template <class T1, class T2> class chain {
+    template <typename T1, typename T2> class chainC {
+    private:
+        T1 p1;
+        T2 p2;
     public:
-        typedef T1* p1;
-        T1 cha1;
-        T2 cha2;
-
-        chain(T1 t1, T2 t2) {
-            this->cha1 = t1;
-            this->cha2 = t2;
+        chainC(T1& t1, T2& t2){
+            p1 = t1;
+            p2 = t2;
         }
+
+        chainC(){}
 
         class iterator {
         private:
-            p1 ptr1;
+            decltype(p1.begin()) p1Start;
+            decltype(p1.end()) p1End;
+            decltype(p2.begin()) p2Start;
+            decltype(p2.end()) p2End;
         public:
-            explicit iterator(p1 ptr1 = nullptr): ptr1(ptr1) {}
 
-            // Note that the method is const as this operator does not
-            // allow changing of the iterator.
-            // Note that it returns T& as it allows to change what it points to.
-            // A const_iterator class will return const T&
-            // and the method will still be const
-            T1& operator*() const {
-                return *ptr1;
+            iterator(T1& p1, T2& p2) {
+                p1Start = p1.begin();
+                p1End = p1.end();
+                p2Start = p2.start();
+                p2End = p2.end();
+            }
+
+            iterator(T1& p1, T2& p2, bool flag) {
+                p1Start = p1.end();
+                p1End = p1.end();
+                p2Start = p2.end();
+                p2End = p2.end();
+            }
+
+            iterator() {}
+
+            auto operator*() const {
+                if(p1Start != p1End) return *p1Start;
+                else return *p2Start;
             }
 
             iterator& operator++() {
-                (*ptr1)++;
+                if(p1Start != p1End) p1Start++;
+                else if(p2Start != p2End) p2Start++;
                 return *this;
             }
 
-            const iterator operator++(int) {
+/*            const iterator operator++(int) {
                 iterator tmp = *this;
-                ptr1++;
+                this++;
                 return tmp;
-            }
+            }*/
 
-            bool operator==(const iterator &it) const {
-                return *ptr1 == *it.ptr;
-            }
+/*            bool operator==(const iterator &it) const {
+                return *this == *it.ptr;
+            }*/
 
             bool operator!=(const iterator& it) const {
-                return *ptr1 != *it.ptr;
+                if (p1Start != it.p1Start) {
+                    return true;
+                }
+                if (p2Start != it.p2Start) {
+                    return true;
+                }
+                return false;
             }
 
         };// END OF CLASS ITERATOR
 
         iterator begin() {
-            return iterator{&cha1};
+            return iterator(p1, p2);
         }
 
         iterator end() {
-            return iterator{&cha2};
+            return iterator(p1, p2, false);
         }
     };
+    template<typename T1, typename T2> chainC<T1, T2> chain(T1 a, T2 b) {
+        return chainC<T1, T2>(a,b);
+    }
 }
 
 #endif //EX5_CHAIN_HPP
--- range.hpp
+++ range.hpp
@@ -8,15 +8,16 @@
 #include "iostream"
 
 namespace itertools {
-    template <typename T> class range {
+    template <typename T> class rangeC {
     public:
-        typedef T* pointer;
         T rng1, rng2;
 
-        range(T rng1, T rng2) {
+        rangeC(T& rng1, T& rng2) {
             this->rng1 = rng1;
             this->rng2 = rng2;
         }
+
+        rangeC(){}
 
         //-------------------------------------------------------------------
         // iterator related code:
@@ -24,49 +25,52 @@ namespace itertools {
         //-------------------------------------------------------------------
         class iterator {
         private:
-            pointer ptr;
+            T ptr;
         public:
-            explicit iterator(pointer ptr = nullptr): ptr(ptr) {}
+            //explicit iterator(T ptr = nullptr): ptr(ptr) {}
 
-            // Note that the method is const as this operator does not
-            // allow changing of the iterator.
-            // Note that it returns T& as it allows to change what it points to.
-            // A const_iterator class will return const T&
-            // and the method will still be const
-            T& operator*() const {
-                return *ptr;
+            iterator() { ptr = nullptr; }
+
+            explicit iterator(T pointer) {
+                ptr = pointer;
+            }
+
+            T operator*() const {
+                return ptr;
             }
 
             iterator& operator++() {
-                (*ptr)++;
+                ptr++;
                 return *this;
             }
 
-            const iterator operator++(int) {
+           const iterator operator++(int) {
                 iterator tmp = *this;
                 ptr++;
                 return tmp;
             }
 
-            bool operator==(const iterator &it) const {
-                return *ptr == *it.ptr;
+             bool operator==(const iterator &it) const {
+                return ptr == it.ptr;
             }
 
             bool operator!=(const iterator& it) const {
-                return *ptr != *it.ptr;
+                return ptr != it.ptr;
             }
 
         };// END OF CLASS ITERATOR
 
         iterator begin() {
-            return iterator{&rng1};
+            return iterator{rng1};
         }
 
         iterator end() {
-            return iterator{&rng2};
+            return iterator{rng2};
         }
-
     };
+      template<typename T> rangeC<T> range(T a, T b) {
+        return rangeC<T>(a,b);
+    }
 }
 
 #endif //EX5_RANGE_HPP

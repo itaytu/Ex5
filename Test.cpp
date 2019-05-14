5caa8fdf3f99258d09ea8517a58d2c65c5dbdce8
itaytu <itaytu93@gmail.com>
2019-05-14 11:19:30 +0300

    Test missing PowerSet

--- Test.cpp
+++ Test.cpp
@@ -58,32 +58,30 @@ int main() {
               powersetC<chainC<rangeC<char>,rangeC<char>>> powChainOfRangChar(charC1);*/
 
         testcase.setname("range")
-                .CHECK_EQUAL(iterable_to_string(intr).compare("1,2,3,4, "), true)
-                .CHECK_EQUAL(iterable_to_string(intr1).compare("4,5,6,7, "), true)
-                .CHECK_EQUAL(iterable_to_string(dour).compare("5.1,6.1,7.1,8.1, "), true)
-                .CHECK_EQUAL(iterable_to_string(charr).compare("a,b,c,d, "), true)
-                .CHECK_EQUAL(iterable_to_string(charr).compare("x,y,z, "), true)
+                .CHECK_OUTPUT(iterable_to_string(intr1),("4,5,6,7,"))
+                .CHECK_OUTPUT(iterable_to_string(dour),("5.1,6.1,7.1,8.1,"))
+                .CHECK_OUTPUT(iterable_to_string(charr),("a,b,c,d,"))
+                .CHECK_OUTPUT(iterable_to_string(charr1),("x,y,"))
                 ;
 
         testcase.setname("chain")
-                .CHECK_EQUAL(iterable_to_string(intC).compare("1,2,3,4,4,5,6,7, "), true)
-                .CHECK_EQUAL(iterable_to_string(charC).compare("a,b,c,d,h,e,l,l,o, "),true)
-                .CHECK_EQUAL(iterable_to_string(charC1).compare("a,b,c,d,x,y, "),true)
+                .CHECK_OUTPUT(iterable_to_string(intC),("1,2,3,4,4,5,6,7,"))
+                .CHECK_OUTPUT(iterable_to_string(charC),("a,b,c,d,h,e,l,l,o,"))
+                .CHECK_OUTPUT(iterable_to_string(charC1),("a,b,c,d,x,y,"))
                 ;
 
         testcase.setname("zip")
-                .CHECK_EQUAL(iterable_to_string(zipIS).compare("10,h,11,e,12,l,13,l,14,o, "), true)
-                .CHECK_EQUAL(iterable_to_string(zipDS).compare("5.1,M,6.1,o,7.1,s,8.1,h,9.1,e, "), true)
-                .CHECK_EQUAL(iterable_to_string(zipII).compare("1,4,2,5,3,6,4,7, "), true)
-                .CHECK_EQUAL(iterable_to_string(zipzip).compare("10,h,5.1,M,11,e,6.1,o,12,l,7.1,s,13,l,8.1,h,14,o,9.1,e, "), true)
+                .CHECK_OUTPUT(iterable_to_string(zipIS),("10,h,11,e,12,l,13,l,14,o,"))
+                .CHECK_OUTPUT(iterable_to_string(zipDS),("5.1,M,6.1,o,7.1,s,8.1,h,9.1,e,"))
+                .CHECK_OUTPUT(iterable_to_string(zipII),("1,4,2,5,3,6,4,7,"))
+                .CHECK_OUTPUT(iterable_to_string(zipzip),("10,h,5.1,M,11,e,6.1,o,12,l,7.1,s,13,l,8.1,h,14,o,9.1,e,"))
                 ;
 
         testcase.setname("product")
-                .CHECK_EQUAL(iterable_to_string(proIntS).compare("1,h,1,e,1,l,1,l,1,o,2,h,2,e,2,l,2,l,2,o,3,h,3,e,3,l,3,l,3,o,4,h,4,e,4,l,4,l,4,o, "), true)
-                .CHECK_EQUAL(iterable_to_string(proDoubleS).compare("5.1,h,5.1,e,5.1,l,5.1,l,5.1,o,6.1,h,6.1,e,6.1,l,6.1,l,6.1,o,7.1,h,7.1,e,7.1,l,7.1,l,7.1,o,8.1,h,8.1,e,8.1,l,8.1,l,8.1,o, "), true)
-                .CHECK_EQUAL(iterable_to_string(proSTRS).compare("h,h,h,e,h,l,h,l,h,o,e,h,e,e,e,l,e,l,e,o,l,h,l,e,l,l,l,l,l,o,l,h,l,e,l,l,l,l,l,o,o,h,o,e,o,l,o,l,o,o, "), true)
-                .CHECK_EQUAL(iterable_to_string(proTwoChains).compare("1,a,1,b,1,c,1,d,1,x,1,y,2,a,2,b,2,c,2,d,2,x,2,y,3,a,3,b,3,c,3,d,3,x,3,y,4,a,4,b,4,c,4,d,4,x,4,y,4,a,4,b,4,c,4,d,4,x,4,y,\n"
-                                                                      "5,a,5,b,5,c,5,d,5,x,5,y,6,a,6,b,6,c,6,d,6,x,6,y,7,a,7,b,7,c,7,d,7,x,7,y, "), true)
+                .CHECK_OUTPUT(iterable_to_string(proIntS),("1,h,1,e,1,l,1,l,1,o,2,h,2,e,2,l,2,l,2,o,3,h,3,e,3,l,3,l,3,o,4,h,4,e,4,l,4,l,4,o,"))
+                .CHECK_OUTPUT(iterable_to_string(proDoubleS),("5.1,h,5.1,e,5.1,l,5.1,l,5.1,o,6.1,h,6.1,e,6.1,l,6.1,l,6.1,o,7.1,h,7.1,e,7.1,l,7.1,l,7.1,o,8.1,h,8.1,e,8.1,l,8.1,l,8.1,o,"))
+                .CHECK_OUTPUT(iterable_to_string(proSTRS),("h,h,h,e,h,l,h,l,h,o,e,h,e,e,e,l,e,l,e,o,l,h,l,e,l,l,l,l,l,o,l,h,l,e,l,l,l,l,l,o,o,h,o,e,o,l,o,l,o,o,"))
+                .CHECK_OUTPUT(iterable_to_string(proTwoChains),("1,a,1,b,1,c,1,d,1,x,1,y,2,a,2,b,2,c,2,d,2,x,2,y,3,a,3,b,3,c,3,d,3,x,3,y,4,a,4,b,4,c,4,d,4,x,4,y,4,a,4,b,4,c,4,d,4,x,4,y,5,a,5,b,5,c,5,d,5,x,5,y,6,a,6,b,6,c,6,d,6,x,6,y,7,a,7,b,7,c,7,d,7,x,7,y,"))
                 ;
 
         grade = testcase.grade();
